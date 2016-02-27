ddbca1a9ec0268079ca40ef8e26b926b15ec6fa7
Minh Bang <contact@minhbang.com>
2016-02-27 08:19:40 +0700

    add en language

---
+++ lang/en/order.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'order'                 => 'Order',
+    'manage'                => 'Manage Order',
+    'manage_title'          => 'Order list <small>sort by updated time</small>',
+    'name'                  => 'Fullname',
+    'email'                 => 'E-mail',
+    'phone'                 => 'Phone',
+    'address'               => 'Address',
+    'statuses'              => 'Status',
+    'update_status_success' => 'Update Order status successfully',
+    'invalid_status_value'  => 'Invalid status value',
+
+    'status'   => [
+        'new'        => 'New',
+        'contacted'  => 'Contacted',
+        'processing' => 'Processing',
+        'canceled'   => 'Canceled',
+        'completed'  => 'Completed',
+    ],
+
+    // show
+    'products' => 'Products',
+    'info'     => 'Information',
+    'price'    => 'Amount',
+];
